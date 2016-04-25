64d7f51ceff0ec2e8552de15c8485950bb7a0446
lubomyr <lubomyr31@gmail.com>
2016-04-25 19:39:55 +0300

    added latest changes 2016-04-14

--- scummvm/AndroidBuild.sh
+++ scummvm/AndroidBuild.sh
@@ -8,9 +8,8 @@ ln -sf libflac.a $LOCAL_PATH/../../../obj/local/$1/libFLAC.a
 ln -sf libvorbis.a $LOCAL_PATH/../../../obj/local/$1/libvorbisfile.a
 ln -sf libtheora.so $LOCAL_PATH/../../../obj/local/$1/libtheoradec.so
 ln -sf libglshim.a $LOCAL_PATH/../../../obj/local/$1/libGL.a
-#ln -sf libnanogl.a $LOCAL_PATH/../../../obj/local/$1/libGL.a
 
 if [ \! -f scummvm/config.mk ] ; then
-	../setEnvironment-$1.sh sh -c "cd scummvm && env LIBS='-lflac -lvorbis -logg -lmad -lz -lgcc -ltheora -lpng -lfreetype -lfaad -lgnustl_static' ./configure --host=androidsdl-$1 --enable-zlib --enable-vorbis --enable-mad --enable-flac --enable-png --enable-theoradec --enable-vkeybd --enable-verbose-build --disable-hq-scalers --disable-readline --disable-nasm --disable-mt32emu --disable-timidity --disable-fluidsynth --enable-all-engines --datadir=. "
+	../setEnvironment-$1.sh sh -c "cd scummvm && env LIBS='-lflac -lvorbis -logg -lmad -lz -lgcc -ltheora -lpng -lfreetype -lfaad -lgnustl_static' ./configure --host=androidsdl-$1 --enable-zlib --enable-vorbis --enable-mad --enable-flac --enable-png --enable-theoradec --enable-vkeybd --enable-verbose-build --disable-readline --disable-nasm --disable-mt32emu --disable-timidity --disable-fluidsynth --opengl-mode=gles --enable-all-engines --datadir=. "
 fi
 ../setEnvironment-$1.sh make -C scummvm -j2 && cp -f scummvm/scummvm libapplication-$1.so
